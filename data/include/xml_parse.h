104e1e1494811bfe3d8dff3cc85ba4cd7f59735f
yangbiao <000111aaaim@gmail.com>
2021-12-14 19:45:42 +0800

    update

--- data/include/xml_parse.h
+++ data/include/xml_parse.h
@@ -1,15 +1,50 @@
 #include <expat.h>
-namespace{
-
-class XmlParse
+#include <string>
+namespace
 {
-private:
-    const char* path="";
-public:
-    XmlParse(const char* path);
-    void parse();
-   
-};
+    struct Object
+    {
+        std::string name;
+        float xmax;
+        float xmin;
+        float ymax;
+        float ymin;
+        int diffcult;
+        int occluded;
+        std::string pose;
+    };
+    struct Size
+    {
+       int depth;
+       int height;
+       int width;
+    };
 
+
+    struct Annotation
+    {
+        std::string image_name;
+        std::string folder;
+        Object object;
+        int segmented;
+        Size size;
+
+
+    };
+
+    
+    
+    
+
+    class XmlParse
+    {
+    private:
+        Annotation annotation;
+        XML_Parser parser;
+    public:
+        XmlParse();
+        ~XmlParse();
+        void parse(const char *path);
+    };
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,69 +8,69 @@
 #include <typeindex>
 using namespace std;
 std::map<std::string, std::any> result;
-void read(rapidxml::xml_node<char> *node)
-{
-    // result[""][""];
+// void read(rapidxml::xml_node<char> *node)
+// {
+//     // result[""][""];
 
-    // result.insert(std::pair<std::string, std::string>("cs","cs"));
-    for (; node != NULL;)
-    {
-        if (node->first_node() != nullptr)
-        {
-            node = node->first_node();
-            // result.insert(std::pair<string, string>(node->name(), node->value()));
-            insert(node, std::pair<string, string>(node->name(), node->value()));
-            read(node);
-        }
+//     // result.insert(std::pair<std::string, std::string>("cs","cs"));
+//     for (; node != NULL;)
+//     {
+//         if (node->first_node() != nullptr)
+//         {
+//             node = node->first_node();
+//             // result.insert(std::pair<string, string>(node->name(), node->value()));
+//             insert(node, std::pair<string, string>(node->name(), node->value()));
+//             read(node);
+//         }
 
-        if (node->next_sibling() != nullptr)
-        {
-            node = node->next_sibling();
-            insert(node, std::pair<string, string>(node->name(), node->value()));
-            read(node);
-        }
+//         if (node->next_sibling() != nullptr)
+//         {
+//             node = node->next_sibling();
+//             insert(node, std::pair<string, string>(node->name(), node->value()));
+//             read(node);
+//         }
 
-        if (node->parent() != nullptr)
-        {
-            node = node->parent();
-            insert(node, std::pair<string, string>(node->name(), node->value()));
-            read(node);
-        }
-    }
-}
+//         if (node->parent() != nullptr)
+//         {
+//             node = node->parent();
+//             insert(node, std::pair<string, string>(node->name(), node->value()));
+//             read(node);
+//         }
+//     }
+// }
 
-void insert(rapidxml::xml_node<char> *node, std::pair<std::any, std::any> data)
-{
-    // rapidxml::xml_node<char> *parent_node;
-    std::vector<std::string> path;
+// void insert(rapidxml::xml_node<char> *node, std::pair<std::any, std::any> data)
+// {
+//     // rapidxml::xml_node<char> *parent_node;
+//     std::vector<std::string> path;
 
-    for (; node != nullptr;)
-    {
-        if (node->parent() != nullptr)
-        {
-            node = node->parent();
-            path.push_back(node->name());
-        }
-    }
+//     for (; node != nullptr;)
+//     {
+//         if (node->parent() != nullptr)
+//         {
+//             node = node->parent();
+//             path.push_back(node->name());
+//         }
+//     }
 
-    for (auto p : path)
-    {
-        if (!result.contains(p))
-        {
-            result.insert(std::pair<std::string, std::any>(p, NULL));
-        }
-    }
+//     for (auto p : path)
+//     {
+//         if (!result.contains(p))
+//         {
+//             result.insert(std::pair<std::string, std::any>(p, NULL));
+//         }
+//     }
 
-    for (auto p : path)
-    {
+//     for (auto p : path)
+//     {
 
-        // auto node = result[p].;
-        if (!result[p].has_value())
-        {
-            // node.insert(data);
-        }
-    }
-}
+//         // auto node = result[p].;
+//         if (!result[p].has_value())
+//         {
+//             // node.insert(data);
+//         }
+//     }
+// }
 
 int main(int argc, char const *argv[])
 {
@@ -80,6 +80,6 @@ int main(int argc, char const *argv[])
     rapidxml::xml_document<> doc;
     doc.parse<0>(file_doc.data());
     // auto node = doc.first_node()->first_node();
-    read(doc.first_node());
+    // read(doc.first_node());
     return 0;
 }
